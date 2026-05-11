a89627f38a15800e8fed99a9ee7b4dd2107ffbdd
aladdin4220434 <aladdingamal044@gmail.com>
2026-05-11 04:27:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -191,5 +191,5 @@ def get_status():
 
 if __name__ == '__main__':
     # إعدادات الأداء لـ Flask
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=False, threaded=True)
